0e7034b71d33303be1bc9fd12a4cf3c080263dfc
Josh LeBlanc <joshleblanc94@gmail.com>
2019-10-03 10:49:44 +0000

    up version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cereal:reactive-render',
-  version: '1.0.3',
+  version: '1.0.4',
   // Brief, one-line summary of the package.
   summary: "A decorator to make a react component's render function reactive",
   // URL to the Git repository containing the source code for this package.
